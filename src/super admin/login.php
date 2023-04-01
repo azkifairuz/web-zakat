f6dbc69c63ff37565182301f04b60723c4c1074e
azkifairuz <azkiajmal@gmail.com>
2023-04-02 02:54:25 +0700

    styling login admin

--- src/super admin/login.php
+++ src/super admin/login.php
@@ -10,15 +10,17 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Admin</title>
+    <link rel="stylesheet" href="../../dist/output.css">
 </head>
 <body>
-    <div>
-        <form method="post">
+    <div class="w-[300px] border border-black rounded-2xl p-6 mx-auto my-[100px]">
+        <h1 class="text-2xl mb-5">Login Admin</h1>
+        <form method="post" class="flex flex-col gap-5">
             <label for="nama">username</label>
-            <input name="nama" placeholder="masukan nama anda" required></input>
+            <input name="nama" placeholder="masukan nama anda" required class="border border-black rounded-lg p-2"></input>
             <label for="pw">Password</label>
-            <input name="pw" type="password" placeholder="masukan password" required></input>
-            <button name="btn-login" type="submit">Login</button>
+            <input name="pw" type="password" placeholder="masukan password"  class="border border-black rounded-lg p-2" required></input>
+            <button name="btn-login" type="submit" class="bg-blue-500 px-4 py-2 text-white rounded-lg">Login</button>
         </form>
         <?php
         if (isset($_POST['btn-login'])) {
