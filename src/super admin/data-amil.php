d85f3443d7e03e572d758e19fafdee9b4d849fcf
azkifairuz <azkiajmal@gmail.com>
2023-04-02 02:38:28 +0700

    styling admin:tambah amil section

--- src/super admin/data-amil.php
+++ src/super admin/data-amil.php
@@ -1,6 +1,9 @@
 <?php
-include("../koneksi.php")
-    ?>
+    include("../koneksi.php");
+    include("session.php");
+    include("navbar.php")
+    
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,29 +12,38 @@ include("../koneksi.php")
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data Amil</title>
+    <link rel="stylesheet" href="../../dist/output.css">
 </head>
 
 <body>
-    <table border=1>
-        <thead>
-            <th>No</th>
-            <th>nama amil</th>
-        </thead>
-        <?php
-            $query = "SELECT * FROM `amil_zakat`";
-            $get = $db->tampil_Data_Banyak($query);
-            $no = 1;
-            foreach ($get as $value) {
-        ?>
-            <tr>
-                <td><?php echo $no ?></td>
-                <td><?php echo $value['nama_amil']?></td>
-            </tr>
-        <?php
-            $no++;
-            }
-        ?>
-    </table>
+    <div class="w-[80%] p-5 bg-gray-200 rounded-lg shadow-lg border border-black mx-auto mt-10 ">
+        <table class="w-full border border-black bg-white rounded-md">
+            <thead class="border border-black">
+                <th class="border border-black">No</th>
+                <th class="border border-black">nama amil</th>
+                <th class="border border-black">Action</th>
+            </thead>
+            <?php
+                $query = "SELECT * FROM `amil_zakat`";
+                $get = $db->tampil_Data_Banyak($query);
+                $no = 1;
+                foreach ($get as $value) {
+            ?>
+                <tr>
+                    <td class="border border-black"><?php echo $no ?></td>
+                    <td class="border border-black w-[50%]"><?php echo $value['nama_amil']?></td>
+                    <td class="border border-black p-2 text-center flex gap-2 w-[30%] justify-center">
+                        <a href="edit-categories.php?p=<?php echo $data['id']; ?>" class=" cursor-pointer text-center rounded-sm  bg-blue-400 text-white py-2 px-4">Edit</a>
+                        <span class="text-2xl block">|</span>
+                        <a href="edit-categories.php?p=<?php echo $data['id']; ?>" class=" cursor-pointer text-center rounded-sm  bg-red-400 text-white py-2 px-4">Hapus</a>
+                    </td>
+                </tr>
+            <?php
+                $no++;
+                }
+            ?>
+        </table>
+    </div>
 </body>
 
 </html>
--- src/super admin/login.php
+++ src/super admin/login.php
@@ -1,5 +1,6 @@
 <?php
-include("../koneksi.php")
+include("../koneksi.php");
+session_start();
 ?>
 
 <!DOCTYPE html>
