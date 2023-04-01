60f335e8da8118be21125fcfa018c43045d79711
azkifairuz <azkiajmal@gmail.com>
2023-04-02 03:08:57 +0700

    styling page pembayaran zakat

---
+++ src/amil zakat/navbar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../dist/output.css">
+</head>
+<body>
+    <nav class="w-screen flex justify-between border-box py-4 px-10 bg-blue-800 text-white items-center">
+        <h1>Admin</h1>
+        <ul class="flex gap-5">
+            <a href="index.php" class="cursor-pointer hover:bg-blue-400 hover:text-black h-full p-2">daftarkan Amil</a>
+            <a href="data-amil.php" class="cursor-pointer hover:bg-blue-400 hover:text-black h-full p-2">Data Amil</a>
+            <a href="data-pembayar.php" class="cursor-pointer hover:bg-blue-400 hover:text-black h-full p-2">Data Pembayar Zakat</a>
+        </ul>
+        <button class="cursor-pointer bg-blue-500 hover:bg-black py-2 px-4 rounded-lg">Logout</button>
+    </nav>
+</body>
+</html>
